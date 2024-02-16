3a617ee7cb67a624623c33e3d182a06b45cd0842
Liam Harmon <93882688+LiamHarmon@users.noreply.github.com>
2024-02-16 19:17:12 +0000

    :tada: Initial Automated Tests

---
+++ fuelbuddy Automation Tests/fuelbuddy_Automation_Tests/src/fuelbuddyAutomatedTests/WelcomeScreenTest.java
@@ -0,0 +1,33 @@
+package fuelbuddyAutomatedTests;
+
+import org.testng.annotations.Test;
+
+import fuelbuddyMainClasses.WelcomeScreen;
+
+import org.testng.Assert;
+
+public class WelcomeScreenTest extends BaseTestClass {
+	
+	@Test
+	public void testFuelBuddyLogo() {
+		WelcomeScreen welcomeScreen = new WelcomeScreen(driver);
+		boolean actualValue = welcomeScreen.isFuelBuddyLogoDisplayed();
+		Assert.assertTrue(actualValue, "The FuelBuddy logo is not displayed.");
+	}
+	
+	@Test
+	public void testWelcomeText() {
+		WelcomeScreen welcomeScreen = new WelcomeScreen(driver);
+        String actualText = welcomeScreen.getWelcomeText();
+        String expectedText = "Welcome";
+        Assert.assertEquals(actualText, expectedText, "The welcome text does not match the expected value.");
+	}
+
+    @Test
+    public void testWelcomeSloganText() {
+        WelcomeScreen welcomeScreen = new WelcomeScreen(driver);
+        String actualText = welcomeScreen.getWelcomeSloganText();
+        String expectedText = "The Friend Your Tank Deserves.";
+        Assert.assertEquals(actualText, expectedText, "The welcome slogan text does not match the expected value.");
+    }
+}
